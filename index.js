73d47e12b553f39434b26be33aebf492064146e9
anzohra@outlook.com <anzohra@outlook.com>
2018-04-04 22:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
-  return 
+  return object
 }
